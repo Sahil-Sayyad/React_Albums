93e7e4022162d3ac643b6c28b0c2ac58f5fc5517
Sahil-Sayyad <96423459+Sahil-Sayyad@users.noreply.github.com>
2024-06-07 03:42:37 +0530

    Update index.jsx

--- src/components/AlbumComponent/index.jsx
+++ src/components/AlbumComponent/index.jsx
@@ -103,7 +103,7 @@ function AlbumComponent() {
                         {albums.map((album) => (
                             <div key={album.id} className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-20 font-mono">
                                 <div className="flex flex-col items-center justify-center pb-10 mt-5">
-                                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="src/assets/profile.png" alt="profile image" />
+                                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://flowbite.com/docs/images/logo.svg" alt="profile image" />
 
                                     <h5 className=" text-center mb-1 text-xl font-medium text-gray-900 dark:text-white">{album.title}</h5>
 
